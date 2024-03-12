db1d1d868c1f40e3abde305a4a45409b58c74e1d
smx1985 <44519248+smx1985@users.noreply.github.com>
2024-03-12 13:05:08 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,11 +155,9 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          GPT￥9.99
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <a href="https://chat.wmbk.net/" target="_blank" class="home_sidebar-sub-title__sbT6Z">请勿公开分享本站</a>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
