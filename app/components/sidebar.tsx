fa07a039ba60b8c51936fb044c67e7b0744bed48
smx1985 <44519248+smx1985@users.noreply.github.com>
2024-03-13 01:56:27 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          GPT￥9.99
+          GPT4.0仅需￥9.99
         </div>
         <div className={styles["sidebar-sub-title"]}>
           <div>
